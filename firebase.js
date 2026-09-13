83f5d9a9f4a6d8763eaf4fe45d4cb79b0fe28384
Yadav Subba <yadavsubba2003@gmail.com>
2026-09-13 11:40:01 +0530

    Update Firebase SDK version and configuration

--- firebase.js
+++ firebase.js
@@ -3,11 +3,9 @@
 // FIREBASE CONFIGURATION
 // =========================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
 import {
-    getDatabase
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
 
 // =========================================
@@ -15,14 +13,30 @@ import {
 // =========================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAoPVLSklKARDfdDoSm6L2zkj1kabJVpsw",
-    authDomain: "rudrabhakti-a1d3e.firebaseapp.com",
-    databaseURL: "https://rudrabhakti-a1d3e-default-rtdb.firebaseio.com",
-    projectId: "rudrabhakti-a1d3e",
-    storageBucket: "rudrabhakti-a1d3e.firebasestorage.app",
-    messagingSenderId: "96491326088",
-    appId: "1:96491326088:web:16b33c95f6aa67b5936d3d",
-    measurementId: "G-RYKGBGSLVB"
+
+    apiKey:
+        "AIzaSyAoPVLSklKARDfdDoSm6Lzkj1kabJVpsw",
+
+    authDomain:
+        "rudrabhakti-a1d3e.firebaseapp.com",
+
+    databaseURL:
+        "https://rudrabhakti-a1d3e-default-rtdb.firebaseio.com",
+
+    projectId:
+        "rudrabhakti-a1d3e",
+
+    storageBucket:
+        "rudrabhakti-a1d3e.firebasestorage.app",
+
+    messagingSenderId:
+        "96491326088",
+
+    appId:
+        "1:96491326088:web:16b33c95f6aa67b5936d3d",
+
+    measurementId:
+        "G-RYKGBGSLVB"
 };
 
 
@@ -30,14 +44,8 @@ const firebaseConfig = {
 // INITIALIZE FIREBASE
 // =========================================
 
-const app = initializeApp(firebaseConfig);
-
-
-// =========================================
-// REALTIME DATABASE
-// =========================================
-
-const database = getDatabase(app);
+const app =
+    initializeApp(firebaseConfig);
 
 
 // =========================================
@@ -45,6 +53,5 @@ const database = getDatabase(app);
 // =========================================
 
 export {
-    app,
-    database
+    app
 };
